89368d5d2af4229cbb56011eced91bbacf541f4b
bautt-s <bauti_hora@hotmail.com>
2023-08-09 18:42:15 -0300

    feat: modified server middleware, enabled cors

--- index.ts
+++ index.ts
@@ -1,16 +1,25 @@
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { expressMiddleware } from '@apollo/server/express4'
 import { ApolloServer } from '@apollo/server'
 import kindeNode from '@kinde-oss/kinde-node'
-import { startStandaloneServer } from '@apollo/server/standalone'
-import { typeDefs } from './src/types.js'
+import bodyParser from 'body-parser'
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+
 import { celebrityMutation, celebrityQuery } from './src/resolvers/celebrity.js'
-import { fanQuery, fanMutation } from './src/resolvers/fan.js'
 import { businessMutation, businessQuery } from './src/resolvers/business.js'
 import { reviewMutation, reviewQuery } from './src/resolvers/review.js'
+import { userMutation, userQuery } from './src/resolvers/user.js'
+import { fanQuery, fanMutation } from './src/resolvers/fan.js'
 import { workMutation } from './src/resolvers/work.js'
 import { resolverMap } from './src/resolvers/date.js'
-import { userMutation, userQuery } from './src/resolvers/user.js'
+import { typeDefs } from './src/types.js'
 
 (async function () {
+    const app = express()
+    const httpServer = http.createServer(app)
+
     const authorize = await kindeNode(process.env.KINDE_DOMAIN)
 
     const resolvers = {
@@ -36,27 +45,36 @@ import { userMutation, userQuery } from './src/resolvers/user.js'
 
     const server = new ApolloServer({
         typeDefs,
-        resolvers
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
     })
 
-    const { url } = await startStandaloneServer(server, {
-        listen: { port: Number.parseInt(process.env.PORT) || 3001 },
-        /*context: async ({ req }) => {
-            // auth check on every request
-            const user = await new Promise((resolve, reject) => {
-                authorize(req, (err: any, user: any) => {
-                    if (err) {
-                        return reject(err);
-                    }
-                    resolve(user);
+    await server.start();
+
+    app.use('/',
+        cors<cors.CorsRequest>(),
+
+        bodyParser.json({ limit: '50mb' }),
+
+        expressMiddleware(server, {
+           /* context: async ({ req }) => {
+                // auth check on every request
+                const user = await new Promise((resolve, reject) => {
+                    authorize(req, (err: any, user: any) => {
+                        if (err) {
+                            return reject(err);
+                        }
+                        resolve(user);
+                    });
                 });
-            });
-        
-            return {
-                user
-            };
-        }*/
-    })
 
-    console.log('Server listening at ' + url)
+                return {
+                    user
+                };
+            }*/
+        }),
+    );
+
+    await new Promise<void>((resolve) => httpServer.listen({ port: Number.parseInt(process.env.PORT) || 3001 }, resolve));
+    console.log(`🚀 Server ready at port ${Number.parseInt(process.env.PORT) || 3001}!`)
 })()
